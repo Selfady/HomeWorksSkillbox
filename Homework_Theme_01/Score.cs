b114d623202fa608e8a552245bc5085316e2df6c
Alexander Saprykin <greedboy@Yandex.ru>
2021-04-19 17:41:33 +0300

    Chanes file location

---
+++ Homework_Theme_01/Score.cs
@@ -0,0 +1,43 @@
+﻿namespace Homework_Theme_01
+/// <summary>
+/// Класс, описывающий имодель баллов пользователя
+/// </summary>
+class Score
+{
+    /// <summary>
+    /// Russian language score
+    /// </summary>
+    public byte RussianLanguage { get; set; }
+
+    /// <summary>
+    /// History score
+    /// </summary>
+    public byte History { get; set; }
+
+    /// <summary>
+    /// Mathematics score
+    /// </summary>
+    public byte Mathematics { get; set; }
+
+    /// <summary>
+    /// Конструктор, позволяющий присвоить значение баллов
+    /// </summary>
+    /// <param name="RussianLanguage">Russian Language score</param>
+    /// <param name="History">History score</param>
+    /// <param name="Mathematics">Mathematics score</param>
+    public Score(byte RussianLanguage, byte History, byte Mathematics)
+    {
+        this.RussianLanguage = RussianLanguage; // Saves set RussianLanguage score
+        this.History = History; // Saves set History score
+        this.Mathematics = Mathematics; // Saves set Mathematics score
+    }
+
+    /// <summary>
+    /// Организация вывода информации о баллах
+    /// </summary>
+    /// <returns>Строковое представление информации</returns>
+    public override string ToString()
+    {
+        return $"Баллы по русскому языку {RussianLanguage} Баллы по истории {History} Баллы по математике {Mathematics}";
+    }
+}
---
+++ Homework_Theme_01/User.cs
@@ -0,0 +1,46 @@
+﻿namespace Homework_Theme_01
+{   
+    /// <summary>
+    /// Класс, описывающий имодель пользователя
+    /// </summary>
+    class User
+    {
+        /// <summary>
+        /// The name of the user
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The age of the worker
+        /// </summary>
+        public byte Age { get; set; }
+
+        /// <summary>
+        /// The height of the worker
+        /// </summary>
+        public ushort Height { get; set; }
+
+        /// <summary>
+        /// Конструктор, позволяющий присвоить значение соответствующим полям пользвателя
+        /// </summary>
+        /// <param name="Name">user name</param>
+        /// <param name="Age">user age</param>
+        /// <param name="Height">user height</param>
+        public User(string Name, byte Age, ushort Height)
+        {
+            this.Name = Name; // Сохранить переданное значение имени
+            this.Age = Age;   // Сохранить переданное значение фамилии
+            this.Height = Height;             // Сохранить переданное значение возраста
+            //this.Score = new Score;       // Сохранить переданное значение зарплаты
+        }
+
+        public override string ToString()
+        {
+            return $"Имя {Name,15} Возраст {Age} Рост {Height}";
+        }
+
+
+    }
+}
+
+
