314dcc75ff40cfcc76126bc5cf57420ee6fdad74
Alexander Saprykin <greedboy@Yandex.ru>
2021-04-22 20:40:54 +0300

    Initial object structure for Homework_Theme_03

---
+++ Homework_Theme_03/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Homework_Theme_03
+{
+    public class Player
+    {
+        /// <summary>
+        /// The name of the player
+        /// </summary>
+        public string PlayerName { get; set; }
+
+        ///// <summary>
+        ///// Flag if the player is a bot(Computer)
+        ///// </summary>
+        //public bool Computer { get; set; }
+
+        public Player(string playerName/*, bool computer*/)
+        {
+            this.PlayerName = playerName;
+            //this.Computer = computer;
+        }
+    }
+}
---
+++ Homework_Theme_03/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Theme_03
+{
+    static class Program
+    {
+        static void Main()
+        {
+            //I have to use unicode for input/output values
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            //General message
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Вы запустили игру \"Homework_Theme_03\n3.10 Домашняя работа\"");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Введите количество игроков:" +
+                              "\n 1 - против компьютера" +
+                              "\n 2 - игра на 2 игрока" +
+                              "\n 3 - игра на 3 игрока" +
+                              "\n 4 - игра на 4 игрока" +
+                              "\n 5 - игра на пятерых");
+            string input = Console.ReadLine();
+            ValidateConsoleInput(input);
+
+
+
+
+
+            Console.ReadLine();
+        }
+
+        private static string ValidateConsoleInput(string input)
+        {
+
+            return input;
+        }
+    }
+}
