d30f66b2d307ebc2adf46ac4801e373eaf80c06a
Alexander Saprykin <greedboy@Yandex.ru>
2021-04-27 17:37:15 +0300

    Pretified Pascal's triangle

--- Homework_Theme_04/Program.cs
+++ Homework_Theme_04/Program.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Homework_Theme_04
 {
 
-    class Finances
+    class Program
     {
         static void Main(string[] args)
         {
+            #region Finances
+
             // A 2D array to store the month report. Has 12 rows and 4 columns.
             int[,] report = new int[12, 4];
 
@@ -90,6 +93,73 @@ namespace Homework_Theme_04
             }
 
             Console.WriteLine("\nThe number of months with positive profit: {0}", numberMonthWithProfit);
+
+            #endregion Finances
+
+            #region Pascal's triangle
+
+            Console.WriteLine("Enter a number of rows for Pascal's triangle");
+            var input = int.TryParse(Console.ReadLine(),out var numberOfStrings);
+            if (!input)
+            {
+                Console.WriteLine("You entered something strange, but i'll show you Pascal's Triangle for 10 rows");
+                numberOfStrings = 10;
+            }
+
+            Console.WriteLine("Pascal's triangle");
+
+            //В строке с номером n (нумерация начинается с 0):
+            //первое и последнее числа равны 1.
+            long current = 1;
+            //https://www.youtube.com/watch?v=lbl9nxwFWDw
+            //(C от n по к) = X*(C от n по k-1), X = (n-k+1)/k
+            for (var n = 0; n < numberOfStrings; n++)
+            {
+                string output = default;
+                for (var k = 0; k <= n; k++)
+                {
+                   current = k == 0 || n == 0 ? 1 : current * (n - k + 1) / k;
+
+                   output = $"{output,10}{current,10}";
+
+                }
+
+                //Console.WriteLine(Console.WindowWidth-20);
+                //Console.WriteLine(output.Length);
+                
+
+                if ((Console.WindowWidth) >= output.Length)
+                {
+                    
+                    Console.SetCursorPosition((Console.WindowWidth - output.Length) / 2, Console.CursorTop);
+                    Console.WriteLine(output);
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("I cannot display more strings properly");
+                    break;
+                }
+            }
+
+            #endregion Pascal's triangle
+
+            #region MatrixOperations
+
+            //default number for matrix scalar multiplication
+            int number = 5;
+
+            //Array[,] int = new int();
+
+            //Реализуйте:
+            //умножение матрицы на число,
+            //  сложение матриц, 
+            //и вычитание 
+            //умножение двух матриц.
+
+            #endregion MatrixOperations
+
 
             Console.ReadLine();
         }
