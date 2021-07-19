81d16b269d12c6f2413da49a4a672fddf3bd8423
Alexander Saprykin <greedboy@Yandex.ru>
2021-07-19 19:29:19 +0300

    Initial commit of obsolete bot sample

---
+++ Homework_Theme_09/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Threading;
+using Newtonsoft.Json.Linq;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using File = System.IO.File;
+
+namespace TODO_Bot
+{
+    class Program
+    {
+        static TelegramBotClient bot;
+
+        static void Main(string[] args)
+        {
+
+            //token to access the bot
+            string token = File.ReadAllText(@"D:\Source\My\Private\Token");
+
+            bot = new TelegramBotClient(token);
+            bot.OnMessage += MessageListener;
+            bot.StartReceiving();
+            Console.ReadKey();
+        }
+
+        private static void MessageListener(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            string text = $"{DateTime.Now.ToLongTimeString()}: {e.Message.Chat.FirstName} {e.Message.Chat.Id} {e.Message.Text}";
+
+            Console.WriteLine($"{text} TypeMessage: {e.Message.Type.ToString()}");
+
+
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Document)
+            {
+                Console.WriteLine(e.Message.Document.FileId);
+                Console.WriteLine(e.Message.Document.FileName);
+                Console.WriteLine(e.Message.Document.FileSize);
+
+                DownLoad(e.Message.Document.FileId, e.Message.Document.FileName);
+            }
+
+            if (e.Message.Text == null) return;
+
+            var messageText = e.Message.Text;
+
+
+            bot.SendTextMessageAsync(e.Message.Chat.Id,
+                $"{messageText}"
+                );
+        }
+
+        static async void DownLoad(string fileId, string path)
+        {
+            var file = await bot.GetFileAsync(fileId);
+            FileStream fs = new FileStream("_" + path, FileMode.Create);
+            await bot.DownloadFileAsync(file.FilePath, fs);
+            fs.Close();
+
+            fs.Dispose();
+        }
+    }
+}
